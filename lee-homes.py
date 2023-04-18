1a7d575741ac917a4ad3717fadd4b48311457849
nduti-lee <lindaashlee06@gmail.com>
2023-04-18 14:13:36 -0700

    lee-homes

--- lee-homes.py
+++ lee-homes.py
@@ -13,3 +13,41 @@ def get_property_list_by_city(city):
     max_pages = 1
     current_page = 1
     filename = city.replace(" ","").replace(",","") + ".csv"
+    if os.path.exists(filename):
+        results_df = pd.read_csv(filename)
+        current_page = ceil(results_df.shape[0]/200) + 1
+        max_pages = current_page + 1
+    else:
+        results_df = pd.DataFrame()
+    while current_page <= max_pages:
+        try:
+            data = get_property_list(
+                coords[0], coords[1], 
+                coords[2], coords[3],
+                current_page=current_page)
+             max_pages = ceil(data["Paging"]["TotalRecords"]/data["Paging"]["RecordsPerPage"])
+            for json in data["Results"]:
+                results_df = results_df.append(pd.json_normalize(json))
+            results_df.to_csv(filename, index=False)
+            current_page += 1
+            sleep(randint(600, 900))  # sleep 10-15 minutes to avoid rate-limit
+        except HTTPError:
+            print("Error occurred on city: " + city)
+            sleep(randint(3000, 3600))  # sleep for 50-60 minutes if limited
+            results_df = pd.read_csv(filename)
+    if "HasDetails" not in results_df.columns:
+        results_df["HasDetails"] = 0
+    for index, row in results_df.iterrows():
+        if row["HasDetails"] == 1: # Avoids re-querying properties that already have details
+            continue
+        property_id = str(row["Id"])
+        mls_reference_number = str(row["MlsNumber"])
+        try:
+            data = get_property_details(property_id, mls_reference_number)
+            results_df = results_df.join(pd.json_normalize(data), lsuffix='_')
+            results_df.loc[index, 'HasDetails'] = 1
+            results_df.to_csv(filename, index=False)
+            sleep(randint(600, 900))  # sleep 10-15 minutes to avoid rate-limit
+        except HTTPError:
+            print("Error occurred on propertyID: " + property_id)
+            sleep(randint(3000, 3600))  # sleep for 50-60 minutes if limited
